15ef46788571cd3fd29ac32b3c638bf40af0e982
thoth2357 <thoth2357@gmail.com>
2024-03-29 16:28:23 +0100

    ✅ Updated test

--- Flight_Booking_API/test_routes.py
+++ Flight_Booking_API/test_routes.py
@@ -1,6 +1,6 @@
 # import fastapi test client
 from fastapi.testclient import TestClient
-from FastAPi_Tutorial.main import app
+from main import app
 
 # Create a test client using the TestClient class
 client = TestClient(app)
@@ -26,21 +26,17 @@ def test_create_booking():
             "seat_pref": "A1",
         },
     )
-    assert response.status_code == 200
+    assert response.status_code == 201
     assert response.json() == {
-        "booking": {
-            "contact_details": {
-                "name": "John Doe",
-                "age": 25,
-                "email": "user@example.com",
-                "phone": "tel:+234-812-345-6789",
-                "next_of_kin": "Jane Doe",
-            },
-            "flight_details": {
-                "origin": "Lagos",
-                "destination": "Abuja",
-                "flight_date": "2021-12-25",
-            },
-            "seat_pref": "A1",
-        }
+        "data": {
+            "customer_name": "John Doe",
+            "customer_age": 25,
+            "customer_email": "user@example.com",
+            "customer_phone": "tel:+234-812-345-6789",
+            "flight_origin": "Lagos",
+            "flight_destination": "Abuja",
+            "flight_date": "2021-12-25",
+            "seat_preference": "A1",
+        },
+        "message": "Booking successful",
     }
