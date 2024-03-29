135df24fd1ced207c97696e4b54b2364376f125f
thoth2357 <thoth2357@gmail.com>
2024-03-29 12:42:45 +0100

    🚧 Added route to create booking

---
+++ routes.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, JSONResponse
+from models import Booking
+from services import process_booking
+booking_router = APIRouter(prefix="/booking", tags=["users"])
+
+
+@booking_router.post("/")
+async def create_booking(booking: Booking):
+    results = await process_booking(booking)
+    return JSONResponse(status_code=201, content={"data": results, "message": "Booking successful"})
+# 
